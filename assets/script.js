13bc39f4895d426ddb68bd29bcea094ec5251fc0
samis <samilsully@gmail.com>
2020-09-15 22:01:16 -0700

    fixed some minor css issues, as well as trying to figure out local storage

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  function userInput() {
+  function weatherGenerate() {
     // This is our API key
     var APIKey = "166a433c57516f51dfab1f7edaed8413";
     // var currentCity = "Seattle";
@@ -10,6 +10,8 @@ $(document).ready(function () {
       currentCity +
       "&appid=" +
       APIKey;
+
+      var currentDate = moment().format("D MMMM YYYY");
 
     // Here we run our AJAX call to the OpenWeatherMap API
     $.ajax({
@@ -24,15 +26,11 @@ $(document).ready(function () {
         var tempF = (response.main.temp - 273.15) * 1.8 + 32;
 
         // var uvI = res.value;
-        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-        $(".tempF").text("Temperature: " + tempF.toFixed(2) + " F");
+        $(".city").html("<h2>" + response.name + " Weather Details</h2>");
+        $(".date").append(currentDate);
         $(".wind").text("Wind Speed: " + response.wind.speed);
         $(".humidity").text("Humidity: " + response.main.humidity);
-        // $(".uvIndex").text("UV Index: " + uvI);
-        // Log the data in the console as well
-        console.log("Wind Speed: " + response.wind.speed);
-        console.log("Humidity: " + response.main.humidity);
-        console.log("Temperature (F): " + tempF);
+        $(".tempF").text("Temperature: " + tempF.toFixed(2) + " F");
 
         var lat = response.coord.lat;
         var lon = response.coord.lon;
@@ -49,33 +47,36 @@ $(document).ready(function () {
           url: queryURL2,
           method: "GET",
         })
-
         .then(function (response) {
-
-        console.log(queryURL2);
-        console.log(response);
-        var currentUVI = $("<span>").text("UVI: ");
-        var currentUVINumber = $("<span>").text(response.current.uvi);
-        $(".uvi").append(currentUVI, currentUVINumber);
-        if (response.current.uvi < 3){
-            currentUVINumber.addClass("uv-green")}
-            else if (response.current.uvi >= 3 && response.current.uvi < 6){
-            currentUVINumber.addClass("uv-yellow")}
-            else if (response.current.uvi >= 6 && response.current.uvi < 8){
-            currentUVINumber.addClass("uv-orange")}
-            else if (response.current.uvi >= 8 && response.current.uvi < 10){
-            currentUVINumber.addClass("uv-red")}
-            else if (response.current.uvi >= 10){
-            currentUVINumber.addClass("uv-violet")}
-        
-        })
-    });
+          console.log(queryURL2);
+          console.log(response);
+          var currentUVI = $("<span>").text("UVI: ");
+          var currentUVINumber = $("<span>").text(response.current.uvi);
+          $(".uvi").append(currentUVI, currentUVINumber);
+          if (response.current.uvi < 3) {
+            currentUVINumber.addClass("uv-green");
+          } else if (response.current.uvi >= 3 && response.current.uvi < 6) {
+            currentUVINumber.addClass("uv-yellow");
+          } else if (response.current.uvi >= 6 && response.current.uvi < 8) {
+            currentUVINumber.addClass("uv-orange");
+          } else if (response.current.uvi >= 8 && response.current.uvi < 10) {
+            currentUVINumber.addClass("uv-red");
+          } else if (response.current.uvi >= 10) {
+            currentUVINumber.addClass("uv-violet");
+          }
+        });
+      });
   }
-  //   create event listener for search button & call userInput function
+  //   create event listener for search button & call weatherGenerate function
   $("#submit-btn").on("click", function (event) {
+    // If I didn't have these empty settings, they would just stack. Not sure why they would, but others wouldn't?
+    $(".uvi").empty();
+    $(".date").empty();
     event.preventDefault();
-    userInput();
+    weatherGenerate();
     console.log(currentCity);
-
+    var storedCities = $(this).attr("previousCities");
+    var userInputCity = $(this).attr().val();
+    localStorage.setItem(storedCities, userInputCity);
   });
 });
